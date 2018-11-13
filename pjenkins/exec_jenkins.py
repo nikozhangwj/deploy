9f501c21cc9cf1486d419fca84d978e4cae2224e
zhangweijie <zhangweijie@wtsd.cn>
2018-11-13 16:11:09 +0800

    fix jenkins url

--- pjenkins/exec_jenkins.py
+++ pjenkins/exec_jenkins.py
@@ -2,7 +2,7 @@ import jenkins
 
 username = 'admin'
 password = 'admin'
-server = jenkins.Jenkins('http://10.128.1.249:8080/login?from=%2F', username=username, password=password)
+server = jenkins.Jenkins('http://192.168.0.124:8081/login?from=%2F', username=username, password=password)
 
 
 def collect_all_job():
