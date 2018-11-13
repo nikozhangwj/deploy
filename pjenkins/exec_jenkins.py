eb95bfeea30948bc723d20f9f28dcb059e8927b5
zhangweijie <zhangweijie@wtsd.cn>
2018-11-13 17:40:07 +0800

    rewrite exec_jenkins

--- pjenkins/exec_jenkins.py
+++ pjenkins/exec_jenkins.py
@@ -6,10 +6,13 @@ class JenkinsWork(object):
     def __init__(self):
         self.username = 'admin'
         self.password = 'admin'
-
-    def collect_all_job(self):
-        server = jenkins.Jenkins(
+        self.server = jenkins.Jenkins(
             'http://192.168.0.124:8081/login?from=%2F', username=self.username, password=self.password
         )
-        all_job = server.get_all_jobs()
+        
+    def collect_all_job(self):
+        all_job = self.server.get_all_jobs()
         return all_job
+
+    def collect_job(self):
+        pass
