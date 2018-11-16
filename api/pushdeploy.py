d536ffe4363c6dc76db23f8c9fa394a41071f604
zhangweijie <zhangweijie@wtsd.cn>
2018-11-16 10:13:46 +0800

    create function does not exist

--- api/pushdeploy.py
+++ api/pushdeploy.py
@@ -6,12 +6,16 @@ from rest_framework.response import Response
 from ..models import DeployList
 from assets.models import AdminUser, Asset
 from django.http import JsonResponse
+from django.core.exceptions import ObjectDoesNotExist
 from ..tasks import test_ansible_ping
 
 
 def get_host_admin(request):
     host = request.GET.get('task_host')
-    asset = Asset.objects.get(ip=host)
-    print(asset)
+    try:
+        asset = Asset.objects.get(ip=host)
+        print(asset)
+    except ObjectDoesNotExist as error:
+        return JsonResponse(dict(code=400, error=str(error)))
     task = test_ansible_ping.delay(asset)
     return JsonResponse(dict(code=200, task=task.id))
