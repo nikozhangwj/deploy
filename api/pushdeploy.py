873e248da5a5836375a13773f2642d38a7a7b084
zhangweijie <zhangweijie@wtsd.cn>
2018-11-16 16:22:37 +0800

    task ok

--- api/pushdeploy.py
+++ api/pushdeploy.py
@@ -31,5 +31,5 @@ def deploy_file_to_asset(request):
         return JsonResponse(dict(code=400, error=str(error)))
 
     task = push_build_file_to_asset_manual(asset, app_name)
-    print(task[0])
+    print(task[0]['ok'])
     return JsonResponse(dict(code=200, task=task))
