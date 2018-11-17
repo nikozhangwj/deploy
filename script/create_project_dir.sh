92dbc49bf5beea810550a72b7c2c20e54b312d4a
zhangweijie <zhangweijie@wtsd.cn>
2018-11-17 12:30:49 +0800

    sh

--- script/create_project_dir.sh
+++ script/create_project_dir.sh
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-# !/bin/bash
-=======
 #!/bin/bash
->>>>>>> f7543d14941cee4b7501a33106e6f571f75b3f48
 
 if [ $# -eq 0 ];then
 echo "empty"
@@ -12,8 +8,4 @@ exit 1;
 fi
 
 APP_NAME=$1
-<<<<<<< HEAD
 mkdir -p /data/${APP_NAME}/{tmp,download,static,secrets,bin,config,logs}
-=======
-mkdir -p /data/${APP_NAME}/{tmp,download,static,secrets,bin,config,logs}
->>>>>>> f7543d14941cee4b7501a33106e6f571f75b3f48
