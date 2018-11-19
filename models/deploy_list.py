8842c3ee5b8f77cfb33f39d63d517c7d9c887620
zhangweijie <zhangweijie@wtsd.cn>
2018-11-19 17:13:23 +0800

    fix too fk error.

--- models/deploy_list.py
+++ models/deploy_list.py
@@ -129,8 +129,8 @@ def turn_build_file_to_deploy(app_name):
 
 def add_version_list(app_name):
     app = DeployList.objects.get(app_name=app_name)
-    # dl = DeployVersion.objects.filter(app_name=app.app_name)
-    # dl.update(symbol=False)
+    dl = DeployVersion.objects.filter(app_name=app.app_name)
+    dl.update(symbol=False)
 
     DeployVersion.objects.create(
         app_name=app,
