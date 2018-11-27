8df2e5518ca44827d7e381cd63a00544ccc49eb0
zhangweijie <zhangweijie@wtsd.cn>
2018-11-27 16:28:43 +0800

    fix file exist error, remove app mkdir function,because jenkins do that before.

--- models/deploy_list.py
+++ models/deploy_list.py
@@ -109,10 +109,14 @@ def create_or_update(queryset):
                 last_success_build_num = 0
             else:
                 last_success_build_num = data.get('last_success_build_num')
+            '''
+            remove mkdir because jenkins is already do this job.
+            
             try:
                 os.makedirs(os.path.join(DeployList.DEPLOY_FILE_DIR, job['name'], 'app'), mode=0o755)
             except FileExistsError as error:
                 print(error)
+            '''
             DeployList.objects.create(
                 app_name=data['app_name'],
                 build_status=data.get('build_status', 'RUNNING'),
