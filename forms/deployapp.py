c3be2b7aba2619c991a8e75f6750c25feb0b2eed
zhangweijie <zhangweijie@wtsd.cn>
2018-12-07 17:28:50 +0800

    APP update view add active select

--- forms/deployapp.py
+++ forms/deployapp.py
@@ -14,11 +14,14 @@ __all__ = []
 class AppUpdateForm(OrgModelForm):
     class Meta:
         model = DeployList
-        fields = ['app_name', 'bound_asset']
+        fields = ['app_name', 'bound_asset', 'job_status']
         widgets = {
             'bound_asset': forms.SelectMultiple(attrs={
                 'class': 'select2', 'data-placeholder': _('bound_asset')
             }),
+            'job_status': forms.Select(attrs={
+                'class': 'select2', 'data-placeholder': _('job_status')
+            })
         }
         labels = {}
         help_texts = {}
