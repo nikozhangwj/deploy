c827e35d032508e67513e02907856cd3d821a2ca
zhangweijie <zhangweijie@wtsd.cn>
2018-12-07 14:04:21 +0800

    rollback - rb function test

--- script/unpack.sh
+++ script/unpack.sh
@@ -4,8 +4,7 @@ backup_path=$1
 backup_directory=$2
 app_name=$3
 
-# cd ${backup_directory}
+cd ${backup_directory}
 
-
-# tar -xvzf ${backup_path}
-# cp -rf ${backup_directory}/data/${app_name}/config /data/${app_name}/config
+tar -xvzf ${backup_path}
+cp -rf ${backup_directory}/data/${app_name}/config /data/${app_name}/config
